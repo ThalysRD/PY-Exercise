09f28e2d867f0e9a36ae917daf1fd73a7173fd36
ThalysRD <tgbr66@gmail.com>
2022-10-07 11:34:15 -0300

    Criação de variáveis, uso de input e transformação e string em integer

---
+++ Aula1.py
@@ -0,0 +1,19 @@
+#Desafio 1 - Crie um script python que leia o nome de uma pessoa e mostre uma mensagem de boas vindas com o valor digitado
+
+nome = input('Qual é o seu nome?')
+print('Seja bem vindo,', nome)
+
+#Desafio 2 - Crie um script python que leia dia, mês e ano de nascimento de uma pessoa e mostre uma mensagem com a data formatada
+
+dia = input('Que dia você nasceu? ')
+mes = input('Que mês você nasceu? ')
+ano = input('Que ano você nasceu? ')
+
+print('Você nasceu no dia', dia, 'de', mes, 'de', ano)
+
+#Desafio 3 - crie um script python que leia dois números e tente mostrar a soma deles
+
+num1 = input('Digite o primeiro número ')
+num2 = input('Digite o segundo número ')
+
+print('A soma dos dois números é', int(num1) + int(num2))
