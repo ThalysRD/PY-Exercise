b7d3e277181064e33572dc4dc78695b641c8dffd
ThalysRD <tgbr66@gmail.com>
2022-10-07 11:48:24 -0300

    Tipos primitivos

---
+++ Aula2.py
@@ -0,0 +1,19 @@
+#Tipos primitivos
+
+n1 = input('Digite um valor: ')
+n2 = int(input('Digite outro valor: '))
+print(type (n1), type (n2))
+
+# O primeiro valor é uma string, e o segundo é um número inteiro
+
+n = float(input('Digite um valor: '))
+print(n)
+
+#O Valor de n é um número com ponto flutuante
+
+#Crie um programa que leia dois números e mostre a soma entre eles
+
+n1 = int(input('Digite um valor: '))
+n2 = int(input('Digite outro valor: '))
+
+print('O resultado da soma entre {} e {} é {}'.format(n1,n2,(n1+n2)))
